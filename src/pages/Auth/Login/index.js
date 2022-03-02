a70aff213e2712a7528bdbba0c17dd3b30a5350a
vigarp <vigar.8421@outlook.com>
2022-03-02 20:26:21 +0700

    create login basic

--- src/pages/Auth/Login/index.js
+++ src/pages/Auth/Login/index.js
@@ -1,8 +1,18 @@
-import React from 'react'
+// import internal modules
+import React from 'react';
+
+// import external modules
+import './login.css'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className="wrapper-login">
+      <div className="d-flex mascot">
+        <img src={require("../../../assets/icons/plane-mascot-authpages.svg").default} alt="plane-mascot-authpages" />
+        <div className="fw-bold fs-4 ms-2">Ankasa</div>
+      </div>
+      {/* continues */}
+    </div>
   )
 }
 
