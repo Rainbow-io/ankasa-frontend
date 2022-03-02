8fef8fe057b56ea5e8b6b4d38c3467433944c2cd
vigarp <vigar.8421@outlook.com>
2022-03-02 20:25:18 +0700

    create basic auth page

--- src/pages/Auth/index.js
+++ src/pages/Auth/index.js
@@ -1,11 +1,20 @@
-import React, { Fragment } from 'react'
+// import internal modules
+import React from 'react'
 import { Outlet } from 'react-router-dom'
+
+// import external modules
+import './index.css'
 
 const Auth = () => {
   return (
-    <Fragment>
-      <Outlet />
-    </Fragment>
+    <div className="min-vh-100 d-flex bg-white">
+      <div className="bg-auth-left w-50">
+        <img className="text-center auth-cover-app" src={require("../../assets/icons/plane-banner-authpages.svg").default} alt="plane-banner-authpages" />
+      </div>
+      <div className="w-50">
+        <Outlet />
+      </div>
+    </div>
   )
 }
 
