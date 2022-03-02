59cd584d257ff846af683be96a5004ac14f02d7c
vigarp <vigar.8421@outlook.com>
2022-03-02 17:29:51 +0700

    setting routes for signup,  auth and 404

--- src/config/Router.js
+++ src/config/Router.js
@@ -1,10 +1,13 @@
 // import internal modules
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 
 // import external modules
+import Page404 from '../pages/404';
+
 import Auth from '../pages/Auth/';
-import Login from '../pages/Auth/Login'
+import Login from '../pages/Auth/Login';
+import SignUp from '../pages/Auth/SignUp';
 
 const Router = () => {
   return (
@@ -12,7 +15,11 @@ const Router = () => {
       <Routes>
         <Route path="/auth" element={<Auth />}>
           <Route path="login" element={<Login />} />
+          <Route path="sign-up" element={<SignUp />} />
+          <Route index element={<Navigate to="/auth/login" />} />
         </Route>
+
+        <Route path="/*" element={<Page404 />} />
       </Routes>
     </BrowserRouter>
   )
