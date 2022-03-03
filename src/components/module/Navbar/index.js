aeb0426e0867391bddfa469b1162f9671536025b
dindachyfr <dindachyfr@gmail.com>
2022-03-02 22:25:23 -0800

    search result page 40%

--- src/components/module/Navbar/index.js
+++ src/components/module/Navbar/index.js
@@ -6,12 +6,12 @@ import Input from '../Input'
 
 const Navbar = () => {
   return (
-    <header className={`${styles.navbar} container-fluid bg-danger`}>
+    <header className={`${styles.navbar} container-fluid`}>
       <div className={`${styles.logo}`}>
         <img src={Logo} alt="" />
         <h4 className={`${styles.titleNavbar}`}>Ankasa</h4>
       </div>
-      <div className={`${styles.midHeader}`}>
+      <div className={`${styles.midHeader} d-flex justify-content-between align-items-center`}>
         <div className={`${styles.navbarSearch}`}>
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class={`bi bi-search ${styles.searchIcon}`} viewBox="0 0 16 16">
             <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
@@ -21,7 +21,7 @@ const Navbar = () => {
             className={`${styles.searchInput}`}
           />
         </div>
-        <h5>Find Ticket</h5>
+        <h5 className='mx-5'>Find Ticket</h5>
         <h5>My Booking</h5>
       </div>
       <div className={`${styles.rightHeader}`}>
