8c596830986f20c9eedcf0c6684685c2bab4766c
vigarp <vigar.8421@outlook.com>
2022-03-10 09:00:52 +0700

    add user context

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -1,8 +1,30 @@
-import React from 'react'
+// import internal modules
+import React, { createContext, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { decodeToken } from 'react-jwt'
 
-const UserContext = () => {
+// import external modules
+import { getUserID } from '../redux/actions/userByID';
+
+export const userContext = createContext(null)
+
+const UserContext = ({ children }) => {
+  const tokenUser = localStorage.getItem("token");
+  const userInfo = decodeToken(tokenUser);
+  const dispatch = useDispatch();
+
+  const userDetailData = useSelector((state) => state.UserID);
+
+  useEffect(() => {
+    dispatch((getUserID(userInfo.id)))
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  console.log(userDetailData)
   return (
-    <div>UserContext</div>
+    <userContext.Provider value={{ userPicture: userDetailData.data?.photo}}>
+      {children}
+    </userContext.Provider>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import UserContext from './context/UserContext';
 import { Provider } from 'react-redux';
 import store from './redux/store'
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-    <App />
+      <UserContext>
+        <App />
+      </UserContext>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
