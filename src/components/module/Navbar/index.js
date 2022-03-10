0ee562c55e2411293021198aab0ccc77c7f4cdca
vigarp <vigar.8421@outlook.com>
2022-03-10 09:02:19 +0700

    add user context in navbar

--- src/components/module/Navbar/index.js
+++ src/components/module/Navbar/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useContext } from 'react'
 import styles from './navbar.module.css'
 import Logo from '../../../assets/plane.svg'
 import Kucing from '../../../assets/kucinggarong.jpeg'
@@ -7,9 +7,12 @@ import { useNavigate } from 'react-router-dom'
 import socket from '../../../helpers/socket' //import socket
 import {decodeToken} from "react-jwt"
 
+import {userContext} from '../../../context/UserContext';
+
 const Navbar = () => {
 
   const navigate = useNavigate()
+  const { userPicture } = useContext(userContext);
 
   //KALO BE UDAH PASANG SOCKET AKAN DI UNCOMMENT
   const token = localStorage.getItem('token')
@@ -49,7 +52,7 @@ const Navbar = () => {
         <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" viewBox="0 0 16 16" className={`${styles.pointer} bi bi-bell`} onClick={()=>navigate("/main/notification")}>
           <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2zM8 1.918l-.797.161A4.002 4.002 0 0 0 4 6c0 .628-.134 2.197-.459 3.742-.16.767-.376 1.566-.663 2.258h10.244c-.287-.692-.502-1.49-.663-2.258C12.134 8.197 12 6.628 12 6a4.002 4.002 0 0 0-3.203-3.92L8 1.917zM14.22 12c.223.447.481.801.78 1H1c.299-.199.557-.553.78-1C2.68 10.2 3 6.88 3 6c0-2.42 1.72-4.44 4.005-4.901a1 1 0 1 1 1.99 0A5.002 5.002 0 0 1 13 6c0 .88.32 4.2 1.22 6z" />
         </svg>
-        <img className={`${styles.profilePic} ${styles.pointer}`} src={Kucing} alt="" onClick={()=>navigate("/main/profile")} />
+        <img className={`${styles.profilePic} ${styles.pointer}`} src={userPicture} alt="" onClick={()=>navigate("/main/profile")} />
       </div>
     </header>
   )
